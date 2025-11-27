26bd60d0998cfc452a257c03c3d6080fe8d76c44
YusukeUtsumi <utsumi.online@gmail.com>
2025-11-27 23:32:37 +0900

    repair astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: "https://github.com/YusukeUtsumi/meltlight-official/",
+  site: "https://studio.meltlight.art/",
 });
